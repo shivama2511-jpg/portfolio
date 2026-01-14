2cc44edc823b88f5dd3ac44a67ed5613e2821f29
Shivam Singh <shivama2511@gmail.com>
2026-01-14 17:46:55 +0530

    added mobile menu js and header

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+  document.querySelector(".menu-toggle").onclick = () => {
+    document.querySelector(".nav-links").classList.toggle("show");
+  };
+</script>
+.nav-links.show {
+  display: flex;
+}
