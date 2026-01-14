dfe8a8ecb433ff2fe4cd1b0c120a048c7ced8d16
Shivam Singh <shivama2511@gmail.com>
2026-01-14 18:16:14 +0530

    fixed mobile menu, header css, whatsapp icon

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-<script>
+
   document.querySelector(".menu-toggle").onclick = () => {
     document.querySelector(".nav-links").classList.toggle("show");
   };
-</script>
-.nav-links.show {
-  display: flex;
-}
+
+
