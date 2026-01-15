6d84190922a726a44c6b989c018e3f4bff618433
Shivam Singh <shivama2511@gmail.com>
2026-01-15 16:15:42 +0530

    Added form and fixed header

--- script.js
+++ script.js
@@ -8,4 +8,13 @@ window.addEventListener("load", () => {
     document.getElementById("intro").style.display = "none";
   }, 3600);
 });
+const openBtn = document.getElementById("form-float-btn");
+const popup = document.getElementById("form-popup");
+const closeBtn = document.getElementById("close-form");
+
+openBtn.onclick = () => popup.style.display = "block";
+closeBtn.onclick = () => popup.style.display = "none";
+setTimeout(() => {
+  document.getElementById("form-float-btn").style.display = "flex";
+}, 5000); // intro 4-0.0 sec ka hai to
 
